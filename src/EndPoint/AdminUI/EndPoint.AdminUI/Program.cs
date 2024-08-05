0f6c23f24dc423c653144f6a70801c41d9b11792
Nima Zare <officialnimazare@gmail.com>
2024-08-05 22:06:27 +0330

    Add project files.

---
+++ src/EndPoint/AdminUI/EndPoint.AdminUI/Program.cs
@@ -0,0 +1,23 @@
+using EndPoint.AdminUI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
