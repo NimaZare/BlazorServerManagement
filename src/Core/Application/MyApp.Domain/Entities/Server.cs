c832262f2b82f7e1edb77a69b85a2b66bec56ca6
Nima Zare <officialnimazare@gmail.com>
2024-08-05 23:11:21 +0330

    add server model

---
+++ src/Core/Application/MyApp.Domain/Entities/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace MyApp.Domain.Entities;
+
+public class Server
+{
+    public Server()
+    {
+        Random random = new();
+        int rndNum = random.Next(0, 2);
+        IsOnline = rndNum != 0;
+    }
+
+    public int Id { get; set; }
+    public bool IsOnline { get; set; }
+    public string? Name { get; set; }
+    public string? City { get; set; }
+}
