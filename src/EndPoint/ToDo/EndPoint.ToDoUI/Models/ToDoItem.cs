03b21f5e1b253f40b493d447e16fa74154dbadce
Nima Zare <officialnimazare@gmail.com>
2024-08-09 13:09:09 +0330

    add new ui for todo list

--- src/EndPoint/ToDo/EndPoint.ToDoUI/Models/ToDoItem.cs
+++ src/EndPoint/ToDo/EndPoint.ToDoUI/Models/ToDoItem.cs
@@ -3,7 +3,24 @@
 public class ToDoItem
 {
     public int Id { get; set; }
+
     public string Name { get; set; } = string.Empty;
-    public bool IsCompleted { get; set; }
+
+    private bool _isCompleted;
+
+    public bool IsCompleted 
+    {
+        get => _isCompleted;
+        set
+        {
+            _isCompleted = value;
+
+            if (value)
+            {
+                DateCompleted = DateTime.Now;
+            }
+        }
+    }
+
     public DateTime DateCompleted { get; set; }
 }
