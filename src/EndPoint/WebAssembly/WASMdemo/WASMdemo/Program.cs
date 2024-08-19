52cbf8a6cf87b7f0912e38b5db98fdb520c0058c
Nima Zare <officialnimazare@gmail.com>
2024-08-19 11:14:19 +0330

    Add WASM project

---
+++ src/EndPoint/WebAssembly/WASMdemo/WASMdemo/Program.cs
@@ -0,0 +1,31 @@
+using WASMdemo.Client.Pages;
+using WASMdemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(WASMdemo.Client._Imports).Assembly);
+
+app.Run();
